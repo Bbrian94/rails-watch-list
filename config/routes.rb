77603bb296aae26b76ac6631b0f74337a4971160
Brian Bennett <brianbennett676@gmail.com>
2022-11-17 15:02:30 +0100

    app_watch_list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'lists/movies/bookmarks'
+  root to: 'lists#index'
+  resources :lists do
+    resources :bookmarks
+  end
+  resources :bookmarks, only: 'destroy'
+  resources :movies
 end
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+    # Defines the root path route ("/")
+    # root "articles#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Lorem.sentence(word_count: 10, supplemental: true, random_words_to_add: 5),
+    poster_url: Faker::LoremFlickr.image,
+    rating: Faker::Number.decimal(l_digits: 2, r_digits: 1)
+  )
+end
